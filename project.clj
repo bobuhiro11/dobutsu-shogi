bc61deab90aad44a081b9ba07c7b205115640927
bobuhiro11 <nob@bobuhiro11.net>
2015-03-15 14:51:05 +0900

    fix

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :main dobutsu-shogi.core
+  :main dobutsu-shogi.gui
   :aot [dobutsu-shogi.gui dobutsu-shogi.core]
   :resource-paths ["resources"]
   :dependencies [[org.clojure/clojure "1.6.0"]
