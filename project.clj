987dce117b391a1c7e49500460cbd3cc42e7fe64
bobuhiro11 <nob@bobuhiro11.net>
2015-04-02 14:55:49 +0900

    fix

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject dobutsu-shogi "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "dobutsu-shogi"
+  :url "http://bobuhiro11.net"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main dobutsu-shogi.gui
